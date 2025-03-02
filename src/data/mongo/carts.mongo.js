e48564a537e8ac63b4f6cdea06d6a30d445fc1ca
Amir kalasnicoy <amirkalasnicoy1812@gmail.com>
2025-03-02 20:48:21 -0300

    agregando el total a pagar

--- src/data/mongo/carts.mongo.js
+++ src/data/mongo/carts.mongo.js
@@ -1,10 +1,12 @@
 import Manager from "./manager.mongo.js";
 import Cart from "./models/carts.model.js";
 import mongoose from "mongoose";
+
 class CartsManager extends Manager {
   constructor() {
     super(Cart);
   }
+
   addProductToCart = async (product_id, user_id, quantity) => {
     try {
       const one = await this.create({ product_id, user_id, quantity });
@@ -31,6 +33,7 @@ class CartsManager extends Manager {
       throw error;
     }
   };
+
   updateState = async (cart_id, state) => {
     try {
       const one = await this.updateById(cart_id, { state });
@@ -39,6 +42,7 @@ class CartsManager extends Manager {
       throw error;
     }
   };
+
   removeProductFromCart = async (cart_id) => {
     try {
       const one = await this.destroyById(cart_id);
@@ -47,8 +51,39 @@ class CartsManager extends Manager {
       throw error;
     }
   };
+
+  totalToPay = async (user_id) => {
+    try {
+      const pipeline = [
+        { $match: { user_id: new mongoose.Types.ObjectId(user_id) } },
+        { $lookup: { from: "products", localField: "product_id", foreignField: "_id", as: "product" } },
+        { $unwind: "$product" },
+        { $lookup: { from: "users", localField: "user_id", foreignField: "_id", as: "user" } },
+        { $unwind: "$user" },
+        { $addFields: { subtotal: { $multiply: ["$product.price", "$quantity"] } } },
+        { $group: {
+            _id: "$user_id",
+            email: { $first: "$user.email" },
+            products: {
+              $push: {
+                title: "$product.title",
+                price: "$product.price",
+                quantity: "$quantity",
+                subtotal: "$subtotal"
+              }
+            },
+            total: { $sum: "$subtotal" }
+        }},
+        { $project: { _id: 0 }}
+      ];
+      const total = await this.model.aggregate(pipeline);
+      return total;
+    } catch (error) {
+      throw error;
+    }
+  };
 }
 
-
+// Exportamos la clase correctamente
 const cartsManager = new CartsManager();
 export default cartsManager;
